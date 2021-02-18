45643c290b88eb865a5294056ad3677981e1b63d
Daniel Almasi <almd@pm.me>
2021-02-18 01:02:04 +0000

    Add lengthMap test
    Add lengthMap test

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,14 @@
-const { gen, generateSecureToken } = require("../index.js");
+const {gen, generateSecureToken} = require("../index.js");
+const lengthMap = require("../length-map.json");
+
+test("sum of byte length combinations in lengthMap matches required bytes", () => {
+    for (const key in lengthMap) {
+        if (Object.hasOwnProperty.call(lengthMap, key)) {
+            const record = lengthMap[key];
+            expect(record.totalBytes).toStrictEqual(record.lengthCombination.reduce((a, b) => a + b, 0));
+        }
+    }
+});
 
 test("token generation algorithm returns a string", () => {
     expect(typeof generateSecureToken(2)).toStrictEqual("string");
@@ -13,30 +23,30 @@ test("gen shorthand calls generateSecureToken function as expected", () => {
 
 test("token generation algorithm throws appropriate error on invalid parameters", () => {
     expect(() => {
-        generateSecureToken(2.45)
+        generateSecureToken(2.45);
     }).toThrow(new Error("Invalid input: must be called with a positive integer."));
 
     expect(() => {
-        generateSecureToken(-5)
+        generateSecureToken(-5);
     }).toThrow(new Error("Invalid input: must be called with a positive integer."));
 
     expect(() => {
-        generateSecureToken("abc")
+        generateSecureToken("abc");
     }).toThrow(new Error("Invalid input: must be called with a positive integer."));
 
     expect(() => {
-        generateSecureToken(23)
+        generateSecureToken(23);
     }).toThrow(new Error("Invalid input: the largest supported length is 20."));
 
     expect(() => {
-        generateSecureToken(2, { avoidModuloBias: "invalid" })
+        generateSecureToken(2, {avoidModuloBias: "invalid"});
     }).toThrow(new Error("Invalid options: avoidModuloBias is a boolean."));
 });
 
 test("token generation algorithm always returns secure random token of expected length", () => {
     for (let length = 1; length <= 8; length++) {
         for (let i = 0; i < 100; i++) {
-            const token = generateSecureToken(length, { avoidModuloBias: false });
+            const token = generateSecureToken(length, {avoidModuloBias: false});
             expect(token.length).toStrictEqual(length);
         }
     }
@@ -45,7 +55,7 @@ test("token generation algorithm always returns secure random token of expected 
 test("token generation algorithm with avoidModuloBias turned on always returns secure random token of expected length", () => {
     for (let length = 1; length <= 8; length++) {
         for (let i = 0; i < 100; i++) {
-            const token = generateSecureToken(length, { avoidModuloBias: true });
+            const token = generateSecureToken(length, {avoidModuloBias: true});
             expect(token.length).toStrictEqual(length);
         }
     }
@@ -54,7 +64,7 @@ test("token generation algorithm with avoidModuloBias turned on always returns s
 test("token generation algorithm always returns a token consisting of digits only", () => {
     for (let length = 1; length <= 8; length++) {
         for (let i = 0; i < 100; i++) {
-            const token = generateSecureToken(length, { avoidModuloBias: false });
+            const token = generateSecureToken(length, {avoidModuloBias: false});
             expect(/^\d+$/.test(token)).toStrictEqual(true);
         }
     }
@@ -63,7 +73,7 @@ test("token generation algorithm always returns a token consisting of digits onl
 test("token generation algorithm with avoidModuloBias turned on always returns a token consisting of digits only", () => {
     for (let length = 1; length <= 8; length++) {
         for (let i = 0; i < 100; i++) {
-            const token = generateSecureToken(length, { avoidModuloBias: true });
+            const token = generateSecureToken(length, {avoidModuloBias: true});
             expect(/^\d+$/.test(token)).toStrictEqual(true);
         }
     }
--- test/stress.test.js
+++ test/stress.test.js
@@ -1,9 +1,9 @@
-const { gen } = require("../index.js");
+const {gen} = require("../index.js");
 
 test("token generation algorithm always returns secure random token of expected length", () => {
     for (let length = 1; length <= 20; length++) {
         for (let i = 0; i < 100; i++) {
-            const token = gen(length, { avoidModuloBias: false });
+            const token = gen(length, {avoidModuloBias: false});
             expect(token.length).toStrictEqual(length);
         }
     }
@@ -12,7 +12,7 @@ test("token generation algorithm always returns secure random token of expected 
 test("token generation algorithm with avoidModuloBias turned on always returns secure random token of expected length", () => {
     for (let length = 1; length <= 20; length++) {
         for (let i = 0; i < 100; i++) {
-            const token = gen(length, { avoidModuloBias: true });
+            const token = gen(length, {avoidModuloBias: true});
             expect(token.length).toStrictEqual(length);
         }
     }
@@ -21,7 +21,7 @@ test("token generation algorithm with avoidModuloBias turned on always returns s
 test("token generation algorithm always returns a token consisting of digits only", () => {
     for (let length = 1; length <= 20; length++) {
         for (let i = 0; i < 100; i++) {
-            const token = gen(length, { avoidModuloBias: false });
+            const token = gen(length, {avoidModuloBias: false});
             expect(/^\d+$/.test(token)).toStrictEqual(true);
         }
     }
@@ -30,7 +30,7 @@ test("token generation algorithm always returns a token consisting of digits onl
 test("token generation algorithm with avoidModuloBias turned on always returns a token consisting of digits only", () => {
     for (let length = 1; length <= 20; length++) {
         for (let i = 0; i < 100; i++) {
-            const token = gen(length, { avoidModuloBias: true });
+            const token = gen(length, {avoidModuloBias: true});
             expect(/^\d+$/.test(token)).toStrictEqual(true);
         }
     }
