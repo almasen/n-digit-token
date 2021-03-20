9c65abe62fae8969002731d79bef22b080944ce2
Daniel Almasi <almd@pm.me>
2021-03-20 20:02:20 +0000

    Add test for running n-digit-token in browser
    Add test for running n-digit-token in browser

---
+++ src/__tests__/byte-stream.test.ts
@@ -0,0 +1,10 @@
+// @ts-nocheck
+import { gen, generateSecureToken } from '../index';
+import { randomBytes } from 'crypto-browserify';
+
+/* eslint-disable max-len */
+
+test('token generation algorithm works with custom byte stream', () => {
+    const token = generateSecureToken(16, { customByteStream: randomBytes });
+    expect(token.length).toStrictEqual(16);
+});
