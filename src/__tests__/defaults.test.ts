4ca1e96ef2fdcaf110ff079c1e861612401a6b19
Daniel Almasi <almd@pm.me>
2023-01-03 22:19:10 +0100

    Add static type checks to jest tests
    Add static type checks to jest tests

--- src/__tests__/defaults.test.ts
+++ src/__tests__/defaults.test.ts
@@ -2,7 +2,8 @@ import { generateSecureToken } from '../generateSecureToken';
 
 test('token generation algorithm returns a string by default', () => {
     for (let length = 1; length <= 32; length++) {
-        expect(typeof generateSecureToken(length)).toStrictEqual('string');
+        const token: string = generateSecureToken(length);
+        expect(typeof token).toStrictEqual('string');
     }
 });
 
--- src/__tests__/return-type.test.ts
+++ src/__tests__/return-type.test.ts
@@ -1,8 +1,8 @@
 import { generateSecureToken } from '../generateSecureToken';
 
 test('setting number as return type returns a number as expected', () => {
-    const token = generateSecureToken(6, { returnType: 'number' });
-    const token2 = generateSecureToken(6, {
+    const token: number = generateSecureToken(6, { returnType: 'number' });
+    const token2: number = generateSecureToken(6, {
         returnType: 'number',
         skipPadding: true,
     });
@@ -11,8 +11,8 @@ test('setting number as return type returns a number as expected', () => {
 });
 
 test('setting integer as return type returns a number as expected', () => {
-    const token = generateSecureToken(6, { returnType: 'integer' });
-    const token2 = generateSecureToken(6, {
+    const token: number = generateSecureToken(6, { returnType: 'integer' });
+    const token2: number = generateSecureToken(6, {
         returnType: 'integer',
         skipPadding: true,
     });
@@ -21,8 +21,8 @@ test('setting integer as return type returns a number as expected', () => {
 });
 
 test('setting bigint as return type returns a bigint as expected', () => {
-    const token = generateSecureToken(6, { returnType: 'bigint' });
-    const token2 = generateSecureToken(6, {
+    const token: bigint = generateSecureToken(6, { returnType: 'bigint' });
+    const token2: bigint = generateSecureToken(6, {
         returnType: 'bigint',
         skipPadding: true,
     });
@@ -31,13 +31,13 @@ test('setting bigint as return type returns a bigint as expected', () => {
 });
 
 test('setting string as return type returns a string as expected', () => {
-    const token0 = generateSecureToken(6);
-    const token1 = generateSecureToken(6, { returnType: 'string' });
-    const token2 = generateSecureToken(6, {
+    const token0: string = generateSecureToken(6);
+    const token1: string = generateSecureToken(6, { returnType: 'string' });
+    const token2: string = generateSecureToken(6, {
         returnType: 'string',
         skipPadding: true,
     });
-    const token3 = generateSecureToken(6, {
+    const token3: string = generateSecureToken(6, {
         returnType: 'string',
         skipPadding: false,
     });
