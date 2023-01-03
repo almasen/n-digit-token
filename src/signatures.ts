c92e22a8526c7df6aaf8f74c6d286f6bfc98b7e0
Daniel Almasi <almd@pm.me>
2023-01-03 22:49:42 +0100

    Update JSDocs of exports
    Update JSDocs of exports

--- src/signatures.ts
+++ src/signatures.ts
@@ -14,11 +14,20 @@ export type TokenGenerator = {
      * This can be customised via the options object, please consult the documentation if interested.
      * @param {number} length desired token length
      * @param {Options} [options] options object (optional)
-     * @param {string} [options.returnType='string'] desired return type (default=string)
+     * @param {string} [options.returnType='string'] desired return type (default=`'string'`)
      * @param {boolean} [options.skipPadding=false] set to true to avoid leading zeros in string tokens (default=false)
      * @param {number} [options.customMemory] memory used in bytes WARNING: Advanced option, use with caution!!
      * @param {number} [options.customByteStream] specify custom CSPRNG byte stream function WARNING: Advanced option, use with caution!!
-     * @return {string|number|bigint} token (as string by default)
+     * @return {string} token (as string by default)
+     * @example
+     *
+     * // ** Default use (recommended) **
+     * const token: string = generateSecureToken(6);
+     * // => '012345' (token is padded with leading zeros if necessary)
+     *
+     * // ** Advanced use **
+     * const tokenWithSkipPadding: string = generateSecureToken(6, { skipPadding: true });
+     * // => '12345' (padding is skipped, WARNING: this may result in tokens with variable length)
      */
     (length: number, options?: OptionsWithDefaultReturnType): string;
 
@@ -31,10 +40,14 @@ export type TokenGenerator = {
      * @param {number} length desired token length
      * @param {Options} [options] options object (optional)
      * @param {string} [options.returnType='string'] desired return type (default=string)
-     * @param {boolean} [options.skipPadding=true] always true with non-string return types
+     * @param {boolean} [options.skipPadding=true] always `true` with non-string return types
      * @param {number} [options.customMemory] memory used in bytes WARNING: Advanced option, use with caution!!
      * @param {number} [options.customByteStream] specify custom CSPRNG byte stream function WARNING: Advanced option, use with caution!!
-     * @return {string|number|bigint} token (as string by default)
+     * @return {number} token (as string by default)
+     * @example
+     *
+     * const token: number = generateSecureToken(6, { returnType: 'number' });
+     * // => 12345 (leading zeros are removed)
      */
     (length: number, options: OptionsWithNumberReturnType): number;
 
@@ -47,10 +60,14 @@ export type TokenGenerator = {
      * @param {number} length desired token length
      * @param {Options} [options] options object (optional)
      * @param {string} [options.returnType='string'] desired return type (default=string)
-     * @param {boolean} [options.skipPadding=true] always true with non-string return types
+     * @param {boolean} [options.skipPadding=true] always `true` with non-string return types
      * @param {number} [options.customMemory] memory used in bytes WARNING: Advanced option, use with caution!!
      * @param {number} [options.customByteStream] specify custom CSPRNG byte stream function WARNING: Advanced option, use with caution!!
-     * @return {string|number|bigint} token (as string by default)
+     * @return {bigint} token (as string by default)
+     * @example
+     *
+     * const token: bigint = generateSecureToken(6, { returnType: 'bigint' });
+     * // => 12345n (leading zeros are removed)
      */
     (length: number, options: OptionsWithBigIntReturnType): bigint;
 };
